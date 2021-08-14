ecdf56f287b9abe987ce6d03a6879f31b4afdae6
Kevin Tayah <ktayah@yahoo.com>
2021-08-14 00:58:10 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,59 @@
+<script>
+	import Navbar from './components/Navbar.svelte';
+</script>
+
+<main>
+	<div class="columns is-desktop">
+		<div class="column">
+			<Navbar />
+		</div>
+		<div class="column is-hidden-touch">
+			<h1 class="title">
+				Hello World
+			  </h1>
+			  <p class="subtitle">
+				My first website with <strong>Bulma</strong>!
+			  </p> 
+		</div>
+	</div>
+</main>
+
+<style lang="scss">
+  @import './assets/styles/global.scss';
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	.header-container {
+		display: flex;
+		flex-direction: row;
+		flex: 1;
+		height: 30rem;
+		background-color: $background;
+
+		.inner-header-container {
+			display: flex;
+			flex: 1;
+			justify-content: center;
+			align-items: center;
+			flex-direction: column;
+		}
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,57 @@
+<script>
+  let active = false;
+
+  function switchActive() {
+    active = !active;
+  }
+</script>
+
+<div>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item">CV</a>
+
+    <a 
+      role="button" 
+      class="navbar-burger" 
+      aria-label="menu" 
+      aria-expanded="false" 
+      data-target="navbarBasicExample" 
+      on:click={switchActive}>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <a class="navbar-item">
+        about
+      </a>
+
+      <a class="navbar-item">
+        resume
+      </a>
+
+      <a class="navbar-item">
+        contact
+      </a>
+    </div>
+  </div>
+  <div class="modal {active && 'is-active'}">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <p>Testing</p>
+    </div>
+    <button class="modal-close is-large" aria-label="close" on:click={switchActive}></button>
+  </div>
+</nav>
+</div>
+
+
+<!-- <style lang="scss">
+  // @import "../assets/styles/global.scss";
+
+}
+</style> -->
