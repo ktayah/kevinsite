7471c943621f6093986925a44e2d285c43faa764
Kevin Tayah <ktayah@yahoo.com>
2022-01-29 15:41:31 -0500

    Fixed up and added skills

--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -1,5 +1,22 @@
 <section>
     <p class="header-text">skills & expertise</p>
+    <ul class="list">
+        <li>HTML/CSS/JavaScript</li>
+        <li>React</li>
+        <li>React Native</li>
+        <li>Redux</li>
+        <li>Svelte</li>
+        <li>Node.js</li>
+        <li>Python</li>
+        <li>C++</li>
+        <li>Rust</li>
+        <li>Elixir</li>
+        <li>Phoenix</li>
+        <li>MongoDB</li>
+        <li>Postgres</li>
+        <li>Git</li>
+        <li>FFMPEG</li>
+    </ul>
 </section>
 
 <style lang="scss">
@@ -10,5 +27,11 @@
         border-bottom-color: black;
         border-bottom-style: solid;
         text-align: left;
+        margin-bottom: 1rem;
+    }
+
+    .list {
+        margin-left: auto;
+        margin-right: auto;
     }
 </style>
