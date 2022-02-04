bbc9c3a734a20dcd9ad7fbaa16d0fd0855416fc1
Kevin Tayah <ktayah@yahoo.com>
2022-02-04 01:25:04 -0500

    Add education

--- src/components/Education.svelte
+++ src/components/Education.svelte
@@ -1,8 +1,13 @@
 <section>
     <p class="header-text">education</p>
+    <p class="is-size-4 has-text-weight-bold">BS in Computer Science, Drexel University, Philadelphia</p>
+    <p class="is-size-6 orange">September 2016 — June 2021</p>
+    <p class="is-size-5">Concentration in Artificial Intelligence, Data Structures, and Algorithms</p>
 </section>
 
 <style lang="scss">
+      @import '../assets/styles/global.scss';
+    
     .header-text {
         font-size: 48px;
         width: 100%;
@@ -10,5 +15,9 @@
         border-bottom-color: black;
         border-bottom-style: solid;
         text-align: left;
+    }
+
+    .orange {
+        color: $accent;
     }
 </style>
