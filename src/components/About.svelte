77833a06b670b4a8b01f5ea2fb905da19baf5d80
Kevin Tayah <ktayah@yahoo.com>
2022-02-05 14:19:51 -0500

    Update about me

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,6 +1,6 @@
 <section id="about">
     <p class="text header-text">about</p>
-    <p class="text">I love building software that are elegant, concise, and most importantly, get the job done. In my past 3 years of experience, I have completed several projects, many in the MERN stack. I found myself wearing multiple hats to fulfill company needs; from scrum master to developer to potential employee recruiter. Sometimes, what keeps servers running is being more than a software developer, but an adaptable team member.</p>
+    <p class="text">I love building software that is elegant, concise, and most importantly, gets the job done. In my past 3 years of experience, I have completed several projects, many in the MERN stack. I found myself wearing multiple hats to fulfill company needs; from scrum master to developer to potential employee recruiter. Sometimes, what keeps servers running is being more than a software developer, but an adaptable team member.</p>
 </section>
 
 <style lang="scss">
