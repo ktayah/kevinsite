ee9d015bc18b062609e5f272269e0cf649a167fe
Kevin Tayah <32495802+ktayah@users.noreply.github.com>
2022-02-16 11:18:02 -0500

    Feature/add experience (#3)
    * Add projects and experience basic
    
    * Fix alignment
    
    * Fix more alignment and
    
    * Fixed up content
    
    * Removed temp modal and moved education
    
    * Removed snapping for mobile; prettify css

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,59 @@
 <script>
-	import 'bulma/css/bulma.scss'
-	import Navbar from './components/Navbar.svelte';
-	import Info from './components/Info.svelte';
-	import About from './components/About.svelte';
-	import Skills from './components/Skills.svelte';
-	import Experience from './components/Experience.svelte';
-	import Education from './components/Education.svelte';
-	import Projects from './components/Projects.svelte';
+  import "bulma/css/bulma.scss";
+  import Navbar from "./components/Navbar.svelte";
+  import Info from "./components/Info.svelte";
+  import About from "./components/About.svelte";
+  import Skills from "./components/Skills.svelte";
+  import Experience from "./components/Experience.svelte";
+  import Education from "./components/Education.svelte";
+  import Projects from "./components/Projects.svelte";
 
-	let modalActive = true;
+  let modalActive = true;
 
-	function switchModalActive() {
-		modalActive = !modalActive;
-	}
+  function switchModalActive() {
+    modalActive = !modalActive;
+  }
 </script>
 
 <main class="has-navbar-fixed-top">
-	<div class="columns is-desktop">
-		<div class="column">
-			<Navbar />
-			<Info />
-			<About />
-			<Skills />
-			<Experience />
-			<Education />
-			<Projects />
-		</div>
-		<div class="column is-hidden-touch">
-			<div class="image" />
-		</div>
-	</div>
-	<!-- Temporary code -->
-	<div class="modal {modalActive && 'is-active'}">
-		<div class="modal-background" on:click={switchModalActive} />
-		<div class="modal-content">
-			<div class="box mx-4">
-				<p class="article my-3">Pardon my appearance, this website is still a work in-progress.</p>
-				<button class="button is-light is-rounded " on:click={switchModalActive}>Okay</button>
-			</div>
-		</div>
-		<button class="modal-close is-large" aria-label="close" on:click={switchModalActive} />
-	</div>
+  <div class="columns is-desktop">
+    <div class="column">
+      <Navbar />
+      <Info />
+      <About />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Education />
+    </div>
+    <div class="column is-hidden-touch">
+      <div class="image" />
+    </div>
+  </div>
 </main>
 
-<style lang="scss">	
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
+<style lang="scss">
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
 
-	.image {
-		position: fixed;
-		bottom: 0;
-		top: 0;
-		right: 0;
-		padding: 0;
-		height: 100%;
-		width: 50%;
-		background-image: url('/static/images/me.png');
-		background-size: cover;
-	}
+  .image {
+    position: fixed;
+    bottom: 0;
+    top: 0;
+    right: 0;
+    padding: 0;
+    height: 100%;
+    width: 50%;
+    background-image: url("/static/images/me.png");
+    background-size: cover;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,14 +1,88 @@
 <section id="projects">
-    <p class="header-text">projects</p>
+  <p class="header-text mb-5">projects</p>
+
+  <div class="block content has-text-left">
+    <p class="is-size-5 has-text-weight-bold mb-0">Personal Website</p>
+    <p class="is-size-6 orange">January 2022 - Present</p>
+    <ul>
+      <li>
+        <b><i>Skills and technologies:</i></b> Svelte, Bulma, SCSS, Vercel
+      </li>
+      <li>
+        Learned Svelte in 2 weeks to build personal website from scratch; also
+        used the Bulma CSS framework and Vercel for deployment.
+      </li>
+      <li>
+        Code available <a
+          class="orange is-size-6"
+          href="https://github.com/ktayah/kevinsite">here</a
+        >.
+      </li>
+    </ul>
+  </div>
+
+  <div class="block content has-text-left">
+    <p class="is-size-5 has-text-weight-bold mb-0">Advent of Code 2021</p>
+    <p class="is-size-6 orange">December 2021 - Present</p>
+    <ul>
+      <li>
+        <b><i>Skills and technologies:</i></b> Rust
+      </li>
+      <li>
+        Solved multiple daily-released coding puzzles ranging in difficulty and
+        requiring multiple types of algorithms.
+      </li>
+      <li>
+        Code available <a
+          class="orange is-size-6"
+          href="https://github.com/ktayah/advent_of_code_2021">here</a
+        >.
+      </li>
+    </ul>
+  </div>
+
+  <div class="block content has-text-left">
+    <p class="is-size-5 has-text-weight-bold mb-0">
+      Drexel Weekend Warriors Club Website
+    </p>
+    <p class="is-size-6 orange">June 2019 - June 2021</p>
+    <ul>
+      <li>
+        <b><i>Skills and technologies:</i></b> Next.js, Redux, Strapi, AWS EC2
+      </li>
+      <li>
+        Full-stack website made for the Weekend Warriors, a club that conducts
+        guided outdoor trips at Drexel University, to advertise trips and allow
+        for member sign-ups.
+      </li>
+      <li>
+        Designed, developed, and deployed website with a complete CMS system to
+        allow client to fully customize website. >.
+      </li>
+      <li>Up to 1000 users have signed up for trips via this website.</li>
+      <li>
+        Code available <a
+          class="orange is-size-6"
+          href="https://github.com/ktayah/drexelWeekendWarriorsSite">here</a
+        >.
+      </li>
+    </ul>
+  </div>
 </section>
 
 <style lang="scss">
-    .header-text {
-        font-size: 48px;
-        width: 100%;
-        border-bottom-width: 1px;
-        border-bottom-color: black;
-        border-bottom-style: solid;
-        text-align: left;
-    }
+  @import "../assets/styles/global.scss";
+
+  .header-text {
+    font-size: 48px;
+    width: 100%;
+    border-bottom-width: 1px;
+    border-bottom-color: black;
+    border-bottom-style: solid;
+    text-align: left;
+  }
+
+  .orange {
+    color: $accent;
+  }
 </style>
