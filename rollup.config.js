a22a501b53a2ffbfddea9cef9b1061de00898eef
Kevin Tayah <ktayah@yahoo.com>
2022-02-16 11:40:15 -0500

    Remove leftover import

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
-import replace from "@rollup/plugin-replace";
 import css from "rollup-plugin-css-only";
 
 const production = !process.env.ROLLUP_WATCH;
